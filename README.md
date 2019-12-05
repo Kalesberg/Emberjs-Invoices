00b785128f563b19f5e99ca68904b5e74f3480f5
Alexander Gill <webdev7575@gmail.com>
2019-12-05 05:34:55 -0800

    Add CRUD UI for invoices using Ember.js

---
+++ README.md
@@ -0,0 +1,5 @@
+
+### Running locally (tested on node.js 8 and 10)
+* `npm install`
+* `npm start`
+* visit http://localhost:4200
---
+++ mirage/fixtures/invoices.js
@@ -0,0 +1,10 @@
+import moment from 'moment';
+import faker from 'faker';
+
+export default [
+  { id: 1, date: moment().format('YYYY-MM-DD'), amount: faker.random.number() },
+  { id: 2, date: moment().format('YYYY-MM-DD'), amount: faker.random.number() },
+  { id: 3, date: moment().format('YYYY-MM-DD'), amount: faker.random.number() },
+  { id: 4, date: moment().format('YYYY-MM-DD'), amount: faker.random.number() },
+  { id: 5, date: moment().format('YYYY-MM-DD'), amount: faker.random.number() },
+];
